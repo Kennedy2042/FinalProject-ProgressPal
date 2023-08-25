d0085c6a0eddf331900279d058c8092cbe98970b
Kennedy2042 <ogbonnakenedy@gmail.com>
2023-08-25 11:35:31 +0100

    Victory Chart added successfully for teacher and admin

--- src/Components/Dashboard/Admin.jsx
+++ src/Components/Dashboard/Admin.jsx
@@ -171,9 +171,9 @@ const Admin = () => {
                                     <p className='AdminDashboardIconsImageName'>Teachers</p>
                                 </div>
                             </div>
-                            <div className="AdminDashboardIcons" onClick={()=>navigate("/Upgrade_now")}>
+                            {/* <div className="AdminDashboardIcons" onClick={()=>navigate("/Upgrade_now")}>
                                         <button>Upgrade</button>
-                                    </div>
+                                    </div> */}
                             {/* <div className="AdminDashboardIcons">
                                 <div className='AdminHomeIcon'>
                                     <MdEmojiEvents size={30} className='AdminDashboardIconsImage' />
